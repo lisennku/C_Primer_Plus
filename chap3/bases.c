18847370357d8d8b1a8ead8a29f87f7c6a360fc1
Lisen Zhang <245062522@qq.com>
2024-08-07 16:03:26 +0800

    chap3

---
+++ chap3/bases.c
@@ -0,0 +1,23 @@
+//
+// Created by lisen on 24-8-7.
+//
+#include <stdio.h>
+int main() {
+	int input, output;
+	input = 100;
+	printf("input in dec = %d, in octal = %o, hex = %x\n", input, input, input);
+	printf("input with prefix in dec = %d, in octal = %#o, hex = %#x\n", input, input, input);
+	printf("input with prefix in dec = %d, in octal = %#o, hex = %#x\n", input, input, input);
+	printf("Now testing backspace\b\b\b\b\baaaaaa\n");
+
+	// printf("Input value in octal\n");
+	// scanf("%o", &output);
+	// printf("input in dec = %d, in octal = %o, hex = %x\n", output, output, output);
+	// printf("input with prefix in dec = %d, in octal = %#o, hex = %#x\n", output, output, output);
+	//
+	// printf("Input value in hex\n");
+	// scanf("%#x", &output);
+	// printf("input in dec = %d, in octal = %o, hex = %x\n", output, output, output);
+	// printf("input with prefix in dec = %d, in octal = %#o, hex = %#x\n", output, output, output);
+
+}
---
+++ chap3/float_point.c
@@ -0,0 +1,9 @@
+//
+// Created by lisen on 24-8-7.
+//
+#include <stdio.h>
+
+int main() {
+	float f = 321.1234;
+	printf("float point number is %f\n", f);
+}
